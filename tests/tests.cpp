903aeaa776a1e961e5789419ef424ef285bb9199
mattys1 <mattys0082@gmail.com>
2025-01-12 17:21:59 +0100

    fix tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,16 @@
 #include <gtest/gtest.h>
+#include "MeasurementsImporter.hpp"
+#include <print>
 
-TEST(ExampleTest, Example) {
-  EXPECT_EQ(1, 1);
+TEST(CsvImport, ReadTest) {
+	std::println("ReadTest");
+	MeasurementsImporter importer;
+	std::vector<MeasurementRecord> records;
+
+	EXPECT_EQ(importer.read_measurements("Chart Export.csv"), std::vector<MeasurementRecord>());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
